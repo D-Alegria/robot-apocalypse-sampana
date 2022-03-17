5068f26b4d23354a90650d1e8b11afd3b7eb8c31
Oladugba Demilade <demiladeoladugba@gmail.com>
2022-03-17 13:24:16 +0100

    init

---
+++ src/main/java/com/sampana/robotapocalypsesampana/RobotApocalypseSampanaApplication.java
@@ -0,0 +1,13 @@
+package com.sampana.robotapocalypsesampana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotApocalypseSampanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotApocalypseSampanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sampana/robotapocalypsesampana/RobotApocalypseSampanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampana.robotapocalypsesampana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotApocalypseSampanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
